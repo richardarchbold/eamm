f5b4c9e15d8ed8a177b22bd8dfc0e6e5825286ac
Richard Archbold <richardarchbold@gmail.com>
2013-01-30 11:06:45 +0000

    added unittest for SurveyResponse.add()

--- eamm/unittests/unittest_survey.py
+++ eamm/unittests/unittest_survey.py
@@ -15,6 +15,33 @@ class TestBackendInvite(unittest.TestCase):
         my_questions2 = eamm.backend.survey.get_questions(id_template2)
         self.assertTrue(my_questions2)
 
+    # we haven't figured out how to test with POST yet, so this will have to do.
+    def test_add1(self):
+        # create a base object
+        my_survey = eamm.backend.survey.SurveyResponse()
+        error_msg = ""
+        
+        # rather than loading the test data from a form, we specify it directly.
+        my_survey.id_invite = 3
+        my_survey.id_meeting = 26
+        my_survey.invitee_email_addr = "rich@rich.com"
+        my_survey.responder_email_addr = "crap@crap.com"
+        my_survey.q_and_a = dict()
+        my_survey.q_and_a[1]  = 50
+        my_survey.q_and_a[2]  = 25
+        my_survey.q_and_a[3]  = 50
+        my_survey.q_and_a[4]  = 75
+        my_survey.q_and_a[7]  = 75
+        my_survey.q_and_a[13] = 50
+        
+        # go ahead and add it, return value should be true.
+        try:
+            ret = my_survey.add()
+        except Exception as e:
+            error_msg = str(e)
+            ret = False
+            
+        self.assertTrue(ret, error_msg)
         
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
